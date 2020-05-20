81db461caf8b8601493fe1b159b263046bb7d9e2
johnny-mh <romz1212@gmail.com>
2020-05-21 03:47:01 +0900

    use google font plugin

--- packages/johnny-mh.github.io/gatsby-browser.js
+++ packages/johnny-mh.github.io/gatsby-browser.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import './src/styles/reset.sass'
 import './src/styles/util.sass'
-import './src/styles/font.sass'
+import './src/styles/font.css'
 import './src/styles/site.sass'
 import './src/styles/prism-vscodedark.css'
 import './src/styles/markdown-content.sass'
--- packages/johnny-mh.github.io/gatsby-config.js
+++ packages/johnny-mh.github.io/gatsby-config.js
@@ -4,5 +4,20 @@ module.exports = {
       resolve: 'gatsby-johnny-shared',
       options: { website: require('./website') },
     },
+    {
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      options: {
+        fonts: [
+          {
+            family: 'Montserrat',
+            variants: ['400', '600'],
+          },
+          {
+            family: 'Noto Sans KR',
+            variants: ['400', '700'],
+          },
+        ],
+      },
+    },
   ],
 }
