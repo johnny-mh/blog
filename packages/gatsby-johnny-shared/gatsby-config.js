b698b0d4727dd2ba928a1499f61ddf02af6e44cb
johnny-mh <romz1212@gmail.com>
2020-07-15 19:21:48 +0900

    fix(gatsby-johnny-shared): react-helmet이 gatsby-config 에 빠져 있어서 추가

--- packages/gatsby-johnny-shared/gatsby-config.js
+++ packages/gatsby-johnny-shared/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = ({ website }) => {
       facebook: website.facebook,
     },
     plugins: [
+      'gatsby-plugin-react-helmet',
       {
         resolve: 'gatsby-source-filesystem',
         options: { path: './src/images' },
